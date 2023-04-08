69bf8da5dbec53f177ccdd371997d3af76237911
malte3d <info@malte3d.com>
2023-04-08 13:58:15 +0200

    Refact: rename class

--- commons/commons-javafx/src/main/java/com/malte3d/suturo/commons/javafx/FxmlLoaderUtil.java
+++ commons/commons-javafx/src/main/java/com/malte3d/suturo/commons/javafx/FxmlLoaderUtil.java
@@ -1,8 +1,6 @@
 package com.malte3d.suturo.commons.javafx;
 
-import java.io.IOException;
-import java.net.URL;
-
+import com.google.common.base.Preconditions;
 import com.google.inject.Injector;
 import com.malte3d.suturo.commons.messages.Messages;
 import javafx.fxml.FXMLLoader;
@@ -10,20 +8,23 @@ import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+import java.net.URL;
+
 @Slf4j
 @UtilityClass
-public final class FXMLUtil {
+public final class FxmlLoaderUtil {
 
     private static Injector injector;
 
     public static void init(@NonNull Injector injector) {
-        FXMLUtil.injector = injector;
+        FxmlLoaderUtil.injector = injector;
     }
 
     @NonNull
     public static <T> T load(@NonNull URL location) {
 
-        validateInit();
+        Preconditions.checkState(injector != null, "Injector has to be initialized with init() before calling this method!");
 
         try {
 
@@ -42,7 +43,7 @@ public final class FXMLUtil {
     @NonNull
     public static <T> T load(@NonNull Object controller, @NonNull URL location) {
 
-        validateInit();
+        Preconditions.checkState(injector != null, "Injector has to be initialized with init() before calling this method!");
 
         try {
 
@@ -60,9 +61,4 @@ public final class FXMLUtil {
         }
     }
 
-    private static void validateInit() {
-
-        if (injector == null)
-            throw new IllegalStateException("Injector has to be initialized with init() before calling this method!");
-    }
 }
